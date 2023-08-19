ca134c1072ff4984a3b5406553ae46e55f50a8b3
Eduardo Hattori <eduardohattorif@gmail.com>
2023-08-19 04:39:13 +0200

    fix 🐛: up

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -3,4 +3,6 @@ vim.cmd([[
 	let g:formatters_typescriptreact = ['eslint_local']
 	let g:formatters_typescript = ['eslint_local']
 	let g:autoformat_verbosemode=0
+	let g:python3_host_prog='/home/hattori/.pyenv/shims/python'
+
 ]])
