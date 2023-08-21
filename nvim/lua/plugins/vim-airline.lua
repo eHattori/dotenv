55f26d26663b24ad936abd00a3e07b224112af27
Eduardo Hattori <eduardohattorif@gmail.com>
2023-08-21 18:42:21 -0300

    fix 🐛: up

--- nvim/lua/plugins/vim-airline.lua
+++ nvim/lua/plugins/vim-airline.lua
@@ -4,5 +4,5 @@ vim.cmd([[
     let g:airline#extensions#branch#enabled=1
     let g:airline_skip_empty_sections = 1
     let g:airline_section_b = '%{airline#util#wrap(airline#extensions#branch#get_head(),0)[0:40]}'
-    let g:airline#extensions#tabline#enabled = 1
+    let g:airline#extensions#tabline#enabled = 0
 ]])
