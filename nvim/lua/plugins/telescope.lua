d18fd06f3eec6121af48d599a3e2efb6486840de
Eduardo Hattori <eduardohattorif@gmail.com>
2024-02-27 20:46:16 -0300

    feat✨: telescope ignore .git

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,1 +1,7 @@
 -- require('telescope').load_extension('dap')
+--
+require('telescope').setup {
+  defaults = {
+file_ignore_patterns = {".git/", ".cache", "%.o", "%.a", "%.out", "%.class",
+		"%.pdf", "%.mkv", "%.mp4", "%.zip"},  }
+}
